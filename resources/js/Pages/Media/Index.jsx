a3716fac55d79c4b86b8292dbf2d2b01a49ac8a2
huzaifatdf <huzaifa.shakeel@thedesignsfirm.com>
2025-05-30 17:43:55 +0500

    build

--- resources/js/Pages/Media/Index.jsx
+++ resources/js/Pages/Media/Index.jsx
@@ -260,7 +260,7 @@ const MediaLibrary = ({ media, filters }) => {
 
             <div className="flex flex-1 flex-col">
         <div className="@container/main flex flex-1 flex-col gap-2 p-10">
-          <div className="w-full ">
+          <div className="w-full space-y-6">
         {/* Header */}
         <div className="flex items-center justify-between">
           <div>
