8ec1dfed3665d8bb285bb2a4d7a4d4170a1f10f1
huzaifatdf <huzaifa.shakeel@thedesignsfirm.com>
2025-06-02 18:18:30 +0500

    Update 2025_06_02_061336_create_forms_table.php

--- database/migrations/2025_06_02_061336_create_forms_table.php
+++ database/migrations/2025_06_02_061336_create_forms_table.php
@@ -32,12 +32,12 @@ return new class extends Migration
             $table->boolean('store_submissions')->default(true);
 
             // User Management
-            $table->unsignedBigInteger('created_by')->nullable();
-            $table->foreign('created_by')->references('id')->on('users')->onDelete('set null');
+           $table->foreignId('created_by')
+            ->nullable()
+            ->constrained('users', 'id')
+            ->onDelete('set null');
 
             $table->timestamps();
-
-            $table->index(['status', 'created_at']);
         });
     }
 
