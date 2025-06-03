349647de05dddcd639819fb6d00fedb6cbbde5af
huzaifatdf <huzaifa.shakeel@thedesignsfirm.com>
2025-06-03 10:20:12 +0500

    Update 2025_06_02_061555_create_form_submissions_table.php

--- database/migrations/2025_06_02_061555_create_form_submissions_table.php
+++ database/migrations/2025_06_02_061555_create_form_submissions_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string('ip_address')->nullable();
             $table->string('user_agent')->nullable();
 
-            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade');
 
 
             // Status
