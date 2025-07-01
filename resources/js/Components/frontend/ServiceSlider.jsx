d6b637df22633de505af084871d7fa4ffb9b09dc
yasirtdf <yasir@thedesignsfirm.com>
2025-07-01 16:01:22 +0500

    Updates

--- resources/js/Components/frontend/ServiceSlider.jsx
+++ resources/js/Components/frontend/ServiceSlider.jsx
@@ -30,19 +30,22 @@ function ServiceSlider() {
         </div>
 
         <Swiper
-          spaceBetween={20}
-          slidesPerView={1}
-          autoplay={{
-            delay: 3000,
-            disableOnInteraction: false,
-          }}
-          breakpoints={{
-            768: { slidesPerView: 2 },
-            1024: { slidesPerView: 5 },
-          }}
-          navigation={true}
-          modules={[Autoplay, Navigation]}
-        >
+            spaceBetween={20}
+            slidesPerView={4}
+            loop={true} // enables infinite loop
+            autoplay={{
+                delay: 0, // continuous movement
+                disableOnInteraction: false,
+                pauseOnMouseEnter: true,
+            }}
+            speed={3000} // smooth speed for transition
+            breakpoints={{
+                768: { slidesPerView: 2 },
+                1024: { slidesPerView: 4 },
+            }}
+            navigation={true}
+            modules={[Autoplay, Navigation]}
+            >
           {services.map((service, index) => (
             <SwiperSlide key="web-apps">
                 <div className="relative w-full h-[420px] overflow-hidden group box services">
@@ -52,7 +55,7 @@ function ServiceSlider() {
                         className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
                     />
                     <div className="absolute inset-0 bg-[#00141b]/80 flex flex-col justify-center items-center text-center px-8 text-inner">
-                        <h3 className="text-[22px] fc-secondary mb-2">{service.short_title}</h3>
+                        <h3 className="text-[22px] fc-secondary mb-2">{service.title}</h3>
                         <p className="text-[14px] fc-white">{service.short_description}</p>
                     </div>
                 </div>
