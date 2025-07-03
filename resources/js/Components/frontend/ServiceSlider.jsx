f5f6caf31f827d5711940968a157515a9da710c8
yasirtdf <yasir@thedesignsfirm.com>
2025-07-03 18:54:45 +0500

    updates

--- resources/js/Components/frontend/ServiceSlider.jsx
+++ resources/js/Components/frontend/ServiceSlider.jsx
@@ -31,40 +31,44 @@ function ServiceSlider() {
           </p>
         </div>
 
-        <Swiper
-            spaceBetween={20}
-            slidesPerView={4}
-            loop={true} // enables infinite loop
-            autoplay={{
-                delay: 0, // continuous movement
-                disableOnInteraction: false,
-                pauseOnMouseEnter: true,
-            }}
-            speed={3000} // smooth speed for transition
-            breakpoints={{
-                0: { slidesPerView: 1 },
-                768: { slidesPerView: 2 },
-                1024: { slidesPerView: 4 },
-            }}
-            navigation={true}
-            modules={[Autoplay, Navigation]}
-            >
-          {services.map((service, index) => (
-            <SwiperSlide key="web-apps">
-                <div className="relative w-full h-[420px] overflow-hidden group box services">
-                    <img
-                        src={`${appUrl}/${service.image}`}
-                        alt={service.title}
-                        className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
-                    />
-                    <div className="absolute inset-0 bg-[#00141b]/80 flex flex-col justify-center items-center text-center px-8 text-inner">
-                        <h3 className="text-[22px] fc-secondary mb-2">{service.title}</h3>
-                        <p className="text-[14px] fc-white">{parse(service.short_description)}</p>
-                    </div>
-                </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        {/* Only render Swiper when services are loaded */}
+        {services.length > 0 && (
+          <Swiper
+              spaceBetween={20}
+              slidesPerView={4}
+              loop={services.length > 4} // Only enable loop if we have more than 4 services
+              autoplay={{
+                  delay: 2000, // 2 seconds delay - more reasonable than 1ms
+                  disableOnInteraction: false,
+                  pauseOnMouseEnter: true,
+              }}
+              speed={1000} // 1 second transition speed
+              breakpoints={{
+                  0: { slidesPerView: 1 },
+                  768: { slidesPerView: 2 },
+                  1024: { slidesPerView: 4 },
+              }}
+              navigation={true}
+              modules={[Autoplay, Navigation]}
+              className="services-swiper"
+          >
+            {services.map((service, index) => (
+              <SwiperSlide key={`service-${service.id || index}`}> {/* Fixed key prop */}
+                  <div className="relative w-full h-[420px] overflow-hidden group box services">
+                      <img
+                          src={`${appUrl}/${service.image}`}
+                          alt={service.title}
+                          className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
+                      />
+                      <div className="absolute inset-0 bg-[#00141b]/80 flex flex-col justify-center items-center text-center px-8 text-inner">
+                          <h3 className="text-[22px] fc-secondary mb-2">{service.title}</h3>
+                          <p className="text-[14px] fc-white">{parse(service.short_description)}</p>
+                      </div>
+                  </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
       </div>
     </div>
   );
