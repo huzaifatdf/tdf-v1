37dddc68a764162ddf3ac29dffc5fd42c714b9d1
huzaifatdf <huzaifa.shakeel@thedesignsfirm.com>
2025-07-07 18:01:35 +0500

    remove app title

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -10,7 +10,7 @@ import { configureEcho } from '@laravel/echo-react';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title || appName}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.jsx`,
